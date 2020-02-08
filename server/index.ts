e3192eea507e58cefdf15fc42ad2b8771b3f1ff3
Kira Burova <coca-cola-from-hell@yandex.ru>
2020-02-08 19:02:07 +0300

    add mlab to the project

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const graphQLHttp = require('express-graphql');
+const dotenv = require('dotenv');
 
 import schema from './schema';
+
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGOLAB_URI, { useMongoClient: true });
 
 app.use('/graphql', graphQLHttp({
     schema,
